f24ce8f1e7963a42047f80e55d649e2a5742ba5c
Lyzev <69327579+Lyzev@users.noreply.github.com>
2026-07-16 15:08:23 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 ## OpenAction System Information plugin
 
-An OpenAction ([OpenDeck](https://github.com/nekename/OpenDeck) / [Tacto](https://tacto.rivul.us)) plugin for displaying system information
+An OpenAction ([OpenDeck](https://github.com/nekename/OpenDeck) / [Tacto](https://tacto.rivul.us)) plugin for displaying Logitech product information
 
 #### Actions
 
-- CPU
-- RAM
-- Uptime
-- OS
+- Mouse Battery
--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,11 @@ use std::collections::HashMap;
 
 use openaction::*;
 
-use sysinfo::System;
-
-struct CPUAction;
+struct BatteryAction;
 #[async_trait]
-impl Action for CPUAction {
-	const UUID: ActionUuid = "me.amankhanna.oasystem.cpu";
+impl Action for BatteryAction {
+	const UUID: ActionUuid = "dev.lyzev.oag.battery";
 	type Settings = HashMap<String, String>;
-}
-
-struct RAMAction;
-#[async_trait]
-impl Action for RAMAction {
-	const UUID: ActionUuid = "me.amankhanna.oasystem.ram";
-	type Settings = HashMap<String, String>;
-}
-
-struct UptimeAction;
-#[async_trait]
-impl Action for UptimeAction {
-	const UUID: ActionUuid = "me.amankhanna.oasystem.uptime";
-	type Settings = HashMap<String, String>;
-}
-
-struct OSAction;
-#[async_trait]
-impl Action for OSAction {
-	const UUID: ActionUuid = "me.amankhanna.oasystem.os";
-	type Settings = HashMap<String, String>;
-
-	async fn will_appear(
-		&self,
-		instance: &Instance,
-		_settings: &Self::Settings,
-	) -> OpenActionResult<()> {
-		instance
-			.set_title(
-				Some(
-					System::long_os_version()
-						.unwrap_or_else(|| "Unknown".to_owned())
-						.replace(" ", "\n"),
-				),
-				None,
-			)
-			.await
-	}
 }
 
 #[tokio::main]
@@ -64,46 +24,27 @@ async fn main() -> OpenActionResult<()> {
 	}
 
 	tokio::spawn(async {
-		let mut system = System::new_all();
+		let mut mouse_battery = "N/A".to_string();
 		loop {
-			system.refresh_cpu_usage();
-			tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-			system.refresh_cpu_usage();
-
-			let cpu_usage = format!("{:.0}%", system.global_cpu_usage());
-			for instance in visible_instances(CPUAction::UUID).await {
-				let _ = instance.set_title(Some(cpu_usage.clone()), None).await;
-			}
-
-			system.refresh_memory();
-			let ram_usage = format!("{:.1}GB", (system.used_memory() as f32) / 1073741824.0);
-			for instance in visible_instances(RAMAction::UUID).await {
-				let _ = instance.set_title(Some(ram_usage.clone()), None).await;
-			}
-
-			{
-				let total_secs = System::uptime();
-				let days = total_secs / 86_400;
-				let hours = (total_secs % 86_400) / 3600;
-				let minutes = (total_secs % 3600) / 60;
-				let seconds = total_secs % 60;
-
-				for instance in visible_instances(UptimeAction::UUID).await {
-					let _ = instance
-						.set_title(
-							Some(format!("{days}d {hours:02}h\n{minutes:02}m {seconds:02}s")),
-							None,
-						)
-						.await;
+			if let Ok(entries) = std::fs::read_dir("/sys/class/power_supply") {
+				for entry in entries.flatten() {
+					let name = entry.file_name().to_string_lossy().into_owned();
+					if name.contains("hidpp_battery") || name.contains("mouse") {
+						let cap_path = entry.path().join("capacity");
+						if let Ok(cap) = std::fs::read_to_string(cap_path) {
+							mouse_battery = format!("{}%", cap.trim());
+							break;
+						}
+					}
 				}
+			}
+			for instance in visible_instances(BatteryAction::UUID).await {
+				let _ = instance.set_title(Some(mouse_battery.clone()), None).await;
 			}
 		}
 	});
 
-	register_action(CPUAction).await;
-	register_action(RAMAction).await;
-	register_action(UptimeAction).await;
-	register_action(OSAction).await;
+	register_action(BatteryAction).await;
 
 	run(std::env::args().collect()).await
 }
